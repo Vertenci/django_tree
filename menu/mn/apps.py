1c7701314e6d98ff0714f369e0ddd7de326c6d36
Undervodkaaa <Vladoc536481@gmail.com>
2025-05-17 01:37:47 +0300

    app

---
+++ menu/mn/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MnConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mn'
---
+++ menu/mn/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название меню")
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    menu = models.ForeignKey(Menu, related_name="items", on_delete=models.CASCADE, verbose_name="Меню")
+    title = models.CharField(max_length=100, verbose_name="Название")
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children',
+                               on_delete=models.CASCADE, verbose_name="Родительский элемент")
+
+    def __str__(self):
+        return self.title
